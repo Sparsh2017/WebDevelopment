e070bdc39c55e07db722627c3b712805ba44c642
“Parthav46” <parthav46@gmail.com>
2019-10-02 17:04:29 +0530

    code refactoring and file reorganisation

---
+++ main.js
@@ -0,0 +1,11 @@
+(function () {
+    var s = document.createElement('script');
+    s.src = "http://www.tickcounter.com/loader.js";
+    s.async = 'async';
+    s.onload = function () {
+        tc_widget_loader('tc_div_89612', 'Countdown', 650, ["1570721000000", "asia-calcutta", "dhms", "FFFFFFF39C123B59983B5998", "0", "3B59981", "HERTZ 2K16"]);
+    };
+    s.onreadystatechange = s.onload;
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(s);
+}());
